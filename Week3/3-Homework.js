13817662c7aa0ff3f9332a1f7452367c185b14ad
Rashad Hubbard <shad.hubbard90@gmail.com>
2023-11-26 23:16:55 -0600

    JSjw

---
+++ Week3/3-Homework.js
@@ -0,0 +1,47 @@
+let pizzaToppings = ["pepperoni", "sausage", "bacon", "bananaPeppers"];
+
+function greetCustomer() {
+for (let topping of pizzaToppings) {
+topping += `${pizzaToppings} `;
+console.log(`Welcome to Pizza House, our toppings are ${topping}`);
+}
+}
+
+//greetCustomer();
+
+
+
+function getPizzaOrder(size, crust, ...pizzaToppings) {
+console.log(`One large stuffed crust pizza with ${topping}`)
+
+getPizzaOrder(size, crust, ...pizzaToppings);
+
+return (size, crust, pizzaToppings);
+}
+
+function preparePizza([large, stuffed, pizza]) {
+console.log("cookingPizza")
+return pizza = {
+  size: "large",
+  crust: "stuffed",
+  toppings: [...pizzaToppings],
+}
+preparePizza([large, stuffed, pizza]);
+}
+
+return pizza = {
+  size: "large",
+  crust: "stuffed",
+  toppings: [...pizzaToppings]
+}
+
+  function servePizza(pizza) {
+    console.log(`Order up! Here's your large stuffed crust pizza with ${topping}, Enjoy!`)
+    return pizza = {
+      size: "large",
+      crust: "stuffed",
+      toppings: [...pizzaToppings],
+    }
+    servePizza(pizza);
+  }
+
